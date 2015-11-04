47e586afdb8598f3a8f13229f24619eb06550c20
Jacob Stuart <stuart4@purdue.edu>
2015-11-03 19:46:50 -0500

    modifies fib function to be tail optimized so that the solution is as efficient as iteration

--- main.ml
+++ main.ml
@@ -1,7 +1,10 @@
- let rec fib n = match n with
-   0 -> 0;
-   | 1 -> 1;
-   | _ -> (fib (n-2)) + (fib (n-1));;
+let fib n =
+        let rec fib_helper n a b =
+                match n with
+                | 0 -> a;
+                | _ -> fib_helper (n-1)  b (a+b);
+        in fib_helper n 0 1;;
 
- let n = int_of_string Sys.argv.(1);;
- print_endline (string_of_int (fib n));;
+
+let n = int_of_string Sys.argv.(1);;
+print_endline (string_of_int (fib n));;
