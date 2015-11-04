84056445bf79675b76344595c78259fc8b00a829
Jacob Stuart <stuart4@purdue.edu>
2015-11-03 19:17:18 -0500

    adds functionality for reading an argument

--- main.ml
+++ main.ml
@@ -3,5 +3,5 @@
    | 1 -> 1;
    | _ -> (fib (n-2)) + (fib (n-1));;
 
- let () =
-         print_endline (string_of_int (fib 6));;
+ let n = int_of_string Sys.argv.(1);;
+ print_endline (string_of_int (fib n));;
